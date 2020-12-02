8a17c680d0b133399089acb5c5c1d94a9939a8b5
nguyentrinhan-dev <nguyentrinhan.dev@gmail.com>
2020-12-02 22:21:03 +0700

    load parameter in form to controller

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+    // can create more requestmapping in a controller
     @RequestMapping("/loginPage")
     String home(Model model){
         return "login";
